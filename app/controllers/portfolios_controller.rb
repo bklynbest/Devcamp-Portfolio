441ed23699173328ac202333dc18a2ef6005778e
Jonathan <bklynbest718@gmail.com>
2018-06-14 23:47:29 -0400

    Implemented initial file uploading via carrierwave for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,35 +13,35 @@ class PortfoliosController < ApplicationController
       Portfolio.find(value[:id]).update(position: value[:position])
     end
 
-    render body: nil
+    render nothing: true
   end
 
   def new
     @portfolio_item = Portfolio.new
-    technology_params
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
+
     respond_to do |format|
-    if @portfolio_item.save
-      format.html {redirect_to portfolios_path, notice: 'Your new post is live'}
-    else
-      format.html {render :new }
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
     end
   end
-end
 
   def edit
-    technology_params
   end
 
   def update
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
-        format.html {redirect_to portfolios_path, notice: 'Portfolio has been updated successfully.'}
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
-        format.html {render :edit}
+        format.html { render :edit }
       end
     end
   end
@@ -50,12 +50,12 @@ end
   end
 
   def destroy
-    #Perform the lookup
-    #destroy/delete the record
+    # Destroy/delete the record
     @portfolio_item.destroy
+
     # Redirect
     respond_to do |format|
-      format.html {redirect_to portfolios_url, notice: 'Record was removed.' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
 
@@ -65,17 +65,13 @@ end
     params.require(:portfolio).permit(:title,
                                       :subtitle,
                                       :body,
+                                      :main_image,
+                                      :thumb_image,
                                       technologies_attributes: [:name]
-                                    )
-  end
-
-  def technology_params
-    @tech_params = 3.times { @portfolio_item.technologies.build }
+                                     )
   end
 
   def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
-
-end
-
+  end
 end
